14f94752db7b4b04700c38aede0c9d9924be157c
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-25 23:46:15 +0700

    Second Commit

---
+++ 16.js
@@ -0,0 +1,13 @@
+//For/Of Loop
+
+//membuat array presiden
+let presiden =
+[
+    "Soekarno", "Soeharto", "Habibie", 
+    "Gus Dur", "Megawati", "SBY", "Jokowi"
+]
+
+//menampilkan seluruh nilai yang tersimpan di array presiden
+for(pres of presiden){
+    console.log(pres);
+}
